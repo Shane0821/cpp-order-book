c3e20854644866beb14305f3c0768865d3081b91
Shane <143796207+Shane0821@users.noreply.github.com>
2025-03-12 22:26:59 +0800

    refactor; add assertion

--- src/book/booktraits.hpp
+++ src/book/booktraits.hpp
@@ -4,68 +4,60 @@
 #include "bookconcepts.hpp"
 #include "order.hpp"
 
-// Primary template
 template <typename LevelContainer>
-struct LevelContainerTraits {};
-
-// deque, list, etc.
-template <typename LevelContainer>
-    requires std::same_as<LevelContainer, std::deque<Order>> ||
-             std::same_as<LevelContainer, std::list<Order>> ||
-             DoubleDirectionLevelContainerConcept<LevelContainer>
 struct LevelContainerTraits {
-    static LevelContainer::iterator insert(LevelContainer& container,
-                                           const Order& order) {
+    static LevelContainer::iterator insert(LevelContainer& container, const Order& order)
+        requires DoubleDirectionLevelContainerConcept<LevelContainer> ||
+                 VectorBasedLevelContainerConcept<LevelContainer>
+    {
         container.push_back(order);
         return std::prev(container.end());
     }
 
-    static void erase(LevelContainer& container, LevelContainer::iterator it) {
-        container.erase(it);
+    static LevelContainer::iterator insert(LevelContainer& container, const Order& order)
+        requires SetBasedContainerConcept<LevelContainer>
+    {
+        auto [it, success] = container.insert(order);
+        return it;
     }
 
-    static LevelContainer::iterator first(LevelContainer& container) {
-        return container.begin();
-    }
-};
-
-// vector, etc.
-template <typename LevelContainer>
-    requires std::same_as<LevelContainer, std::vector<Order>> ||
-             VectorBasedLevelContainerConcept<LevelContainer>
-struct LevelContainerTraits {
-    static LevelContainer::iterator insert(LevelContainer& container,
-                                           const Order& order) {
-        container.push_back(order);
-        return std::prev(container.end());
-    }
-
-    static void erase(LevelContainer& container, LevelContainer::iterator it) {
-        container.erase(it);
-    }
-
-    static LevelContainer::iterator first(LevelContainer& container) {
-        return std::prev(container.end());
-    }
-};
-
-// std::set, std::unordered_set, etc.
-template <typename LevelContainer>
-    requires LevelContainerBaseConcept<LevelContainer>
-struct LevelContainerTraits {
-    static LevelContainer::iterator insert(LevelContainer& container,
-                                           const Order& order) {
+    static LevelContainer::iterator insert(LevelContainer& container, const Order& order)
+        requires MultiSetBasedContainerConcept<LevelContainer>
+    {
         auto it = container.insert(order);
         return it;
     }
 
-    static void erase(LevelContainer& container, typename LevelContainer::iterator it) {
+    static void erase(LevelContainer& container, LevelContainer::iterator it) {
         container.erase(it);
     }
 
-    static LevelContainer::iterator first(LevelContainer& container) {
+    static LevelContainer::iterator first(LevelContainer& container)
+        requires(!VectorBasedLevelContainerConcept<LevelContainer>)
+    {
         return container.begin();
     }
+
+    static LevelContainer::iterator first(LevelContainer& container)
+        requires VectorBasedLevelContainerConcept<LevelContainer>
+    {
+        return std::prev(container.end());
+    }
 };
+
+// assertions
+template <typename T>
+constexpr bool has_valid_insert_v = requires(T container, const Order& order) {
+    {
+        LevelContainerTraits<T>::insert(container, order)
+    } -> std::same_as<typename T::iterator>;
+};
+
+static_assert(has_valid_insert_v<std::vector<Order>>, "vector has valid insert");
+static_assert(has_valid_insert_v<std::deque<Order>>, "deque has valid insert");
+static_assert(has_valid_insert_v<std::list<Order>>, "list has valid insert");
+static_assert(has_valid_insert_v<std::set<Order>>, "set has valid insert");
+static_assert(has_valid_insert_v<std::unordered_set<Order>>,
+              "unordered_set has valid insert");
 
 #endif  // _BOOK_TRAITS_HPP
