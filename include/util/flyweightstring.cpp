742f7bca1a0044a3d0781914c4c70ad36a1cb56c
Shane <143796207+Shane0821@users.noreply.github.com>
2025-03-13 11:45:16 +0800

    fix deadlock

--- include/util/flyweightstring.cpp
+++ include/util/flyweightstring.cpp
@@ -1,27 +1,21 @@
 #include "flyweightstring.h"
 
-FlyweightString::FlyweightString(const std::string& str) {
-    std::lock_guard<std::mutex> lock(mutex);
-    value = getOrAdd(str);
-}
+FlyweightString::FlyweightString(const std::string& str) { value = getOrAdd(str); }
 
-FlyweightString::FlyweightString(std::string&& str) {
-    std::lock_guard<std::mutex> lock(mutex);
-    value = getOrAdd(std::move(str));
-}
+FlyweightString::FlyweightString(std::string&& str) { value = getOrAdd(std::move(str)); }
 
 std::shared_ptr<std::string> FlyweightString::getOrAdd(const std::string& str) {
+    std::lock_guard<std::mutex> lock(mutex);
     if (auto it = pool.find(str); it != pool.end()) {
         return it->second;
     }
-    std::lock_guard<std::mutex> lock(mutex);
     return pool.emplace(str, std::make_shared<std::string>(str)).first->second;
 }
 
 std::shared_ptr<std::string> FlyweightString::getOrAdd(std::string&& str) {
+    std::lock_guard<std::mutex> lock(mutex);
     if (auto it = pool.find(str); it != pool.end()) {
         return it->second;
     }
-    std::lock_guard<std::mutex> lock(mutex);
     return pool.emplace(str, std::make_shared<std::string>(std::move(str))).first->second;
 }
