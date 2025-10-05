3c6efc8a436991e0f09c8a30ab8f1229d67d6a26
Shane <143796207+Shane0821@users.noreply.github.com>
2025-10-05 10:20:25 +0800

    refactor: update order class

---
+++ src/book/order.h
@@ -0,0 +1,43 @@
+#ifndef _ORDER_H
+#define _ORDER_H
+
+#include "constants.h"
+#include "order_type.h"
+#include "side.h"
+#include "usings.h"
+
+struct Order {
+   public:
+    Order() = default;
+
+    Order(OrderId orderID, OrderType type, Side side, Price price, Quantity quantity)
+        : orderId_(orderID),
+          side_(side),
+          type_(type),
+          initialQuantity_(quantity),
+          remainingQuantity_(quantity),
+          price_(price) {}
+
+    Order(OrderId orderId, Side side, Quantity quantity)
+        : Order(orderId, OrderType::Market, side, Constants::InvalidPrice, quantity) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Order& order) {
+        return os << "OrderID: " << order.orderId_
+                  << ", Side: " << (order.side_ == Side::Buy ? "Buy" : "Sell")
+                  << ", Initial Quantity: " << order.initialQuantity_
+                  << ", Remaining Quantity: " << order.remainingQuantity_
+                  << ", Price: " << order.price_ << std::endl;
+    }
+
+    bool operator==(const Order& other) const { return orderId_ == other.orderId_; }
+    bool operator!=(const Order& other) const { return !(*this == other); }
+
+    Side side_;
+    OrderType type_;
+    OrderId orderId_;
+    Quantity initialQuantity_{0};
+    Quantity remainingQuantity_{0};
+    Price price_{0};
+};
+
+#endif  // _ORDER_H
