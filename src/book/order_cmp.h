f75238327a32ffc36c39a7537a74d99c2bb972fd
Shane <143796207+Shane0821@users.noreply.github.com>
2025-11-02 15:04:43 +0800

    feat: order equal and order hash

--- src/book/order_cmp.h
+++ src/book/order_cmp.h
@@ -1,13 +1,30 @@
 #pragma once
 
+#include <functional>
+
 #include "order.h"
 
-struct OrderComparison {
+struct OrderCompare {
     bool operator()(const Order &a, const Order &b) const {
         return a.creationTime_ < b.creationTime_;
     }
 
     bool operator()(const Order *a, const Order *b) const {
         return a->creationTime_ < b->creationTime_;
+    }
+};
+
+struct OrderEqual {
+    bool operator()(const Order &a, const Order &b) const { return a == b; }
+
+    bool operator()(const Order *a, const Order *b) const { return *a == *b; }
+};
+
+struct OrderHash {
+    size_t operator()(const Order &a) const {
+        return std::hash<std::string>{}(a.orderId_);
+    }
+    size_t operator()(const Order *a) const {
+        return std::hash<std::string>{}(a->orderId_);
     }
 };
