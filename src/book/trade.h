8e71e99453bb96ddf1613239e3828f238f94032d
Shane <143796207+Shane0821@users.noreply.github.com>
2025-11-02 16:15:52 +0800

    refactor: use trade struct

--- src/book/trade.h
+++ src/book/trade.h
@@ -8,15 +8,10 @@ struct TradeInfo {
     Quantity quantity_;
 };
 
-class Trade {
-   public:
+struct Trade {
     Trade(const TradeInfo &bidTrade, const TradeInfo &askTrade)
         : bidTrade_{bidTrade}, askTrade_{askTrade} {}
 
-    const TradeInfo &GetBidTrade() const { return bidTrade_; }
-    const TradeInfo &GetAskTrade() const { return askTrade_; }
-
-   private:
     TradeInfo bidTrade_;
     TradeInfo askTrade_;
 };
